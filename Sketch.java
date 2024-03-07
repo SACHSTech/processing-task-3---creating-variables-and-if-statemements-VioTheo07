006f528c8a044e0d67c39ccbe29060f8ccbdf4e0
yourGithubUsername <yourGithubAccountEmail>
2024-03-06 20:28:32 -0500

    Submit

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,93 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
-  public void settings() {
-	// put your size call here
-    size(400, 400);
-  }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
   
-  // define other methods down here.
+  float fltRandomX = 0;
+  float fltRandomY = 0;
+
+  int intHouseColourR = 0;
+  int intHouseColourG = 0;
+  int intHouseColourB = 0;
+
+  int intBackgroundColourR = 0;
+  int intBackgroundColourG = 0;
+  int intBackgroundColourB = 0;
+
+  public void settings() {
+    size(900, 800);
+  }
+
+  public void setup() {
+    fltRandomX = random(0, 700);
+    fltRandomY = random(0, 510);
+    
+    // Random house color
+    intHouseColourR = (int) random(256);
+    intHouseColourG = (int) random(256);
+    intHouseColourB = (int) random(256);
+
+    // Random background color
+    intBackgroundColourR = (int) random(256);
+    intBackgroundColourG = (int) random(256);
+    intBackgroundColourB = (int) random(256);
+
+    background(intBackgroundColourR, intBackgroundColourG, intBackgroundColourB);
+    
+    if ((fltRandomX <= 850 / 3) && (fltRandomY <= 750 / 2)) {
+      intHouseColourR = (int) random(256);
+      intHouseColourG = (int) random(256);
+      intHouseColourB = (int) random(256);
+    } 
+    else if ((fltRandomX >= (850 / 3) * 2) && (fltRandomY <= 750 / 2)) {
+      intHouseColourR = (int) random(256);
+      intHouseColourG = (int) random(256);
+      intHouseColourB = (int) random(256);
+    } 
+    else if ((fltRandomX <= (850 / 3) * 2) && (fltRandomX >= 850 / 3) && (fltRandomY <= 750 / 2)) {
+      intHouseColourR = (int) random(256);
+      intHouseColourG = (int) random(256);
+      intHouseColourB = (int) random(256);
+    }
+    else if ((fltRandomX >= (850 / 3) * 2) && (fltRandomY >= 750 / 2)) {
+      intHouseColourR = (int) random(256);
+      intHouseColourG = (int) random(256);
+      intHouseColourB = (int) random(256);
+    }
+    else if ((fltRandomX <= 850 / 3) && (fltRandomY >= 750 / 2)) {
+      intHouseColourR = (int) random(256);
+      intHouseColourG = (int) random(256);
+      intHouseColourB = (int) random(256);
+    }   
+    else {
+      intHouseColourR = (int) random(256);
+      intHouseColourG = (int) random(256);
+      intHouseColourB = (int) random(256);
+    } 
+    
+    // house
+    // base and ceiling
+    stroke(intHouseColourR, intHouseColourG, intBackgroundColourB);
+    line(25 + fltRandomX, 115 + fltRandomY, 175 + fltRandomX, 115 + fltRandomY);
+    line(25 + fltRandomX, 265 + fltRandomY, 175 + fltRandomX, 265 + fltRandomY);
+    // walls
+    line(25 + fltRandomX, 115 + fltRandomY, 25 + fltRandomX, 265 + fltRandomY);
+    line(175 + fltRandomX, 115 + fltRandomY, 175 + fltRandomX, 265 + fltRandomY);
+    // roof
+    line(25 + fltRandomX, 115 + fltRandomY, 100 + fltRandomX, 25 + fltRandomY);
+    line(175 + fltRandomX, 115 + fltRandomY, 100 + fltRandomX, 25 + fltRandomY);
+    // door
+    line(75 + fltRandomX, 265 + fltRandomY, 75 + fltRandomX, 190 + fltRandomY);
+    line(125 + fltRandomX, 265 + fltRandomY, 125 + fltRandomX, 190 + fltRandomY);
+    line(75 + fltRandomX, 190 + fltRandomY, 125 + fltRandomX, 190 + fltRandomY);
+    // doorknob
+    line(107 + fltRandomX, 225 + fltRandomY, 117 + fltRandomX, 225 + fltRandomY);
+    line(107 + fltRandomX, 235 + fltRandomY, 117 + fltRandomX, 235 + fltRandomY);
+    line(107 + fltRandomX, 225 + fltRandomY, 107 + fltRandomX, 235 + fltRandomY);
+    line(117 + fltRandomX, 225 + fltRandomY, 117 + fltRandomX, 235 + fltRandomY);
+
+    // Display time
+    textSize(32);
+    fill(255);
+    text("Time: " + hour() + ":" + minute() + ":" + second(), 20, 50);
+  }
 }
